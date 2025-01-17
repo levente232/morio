4a1d37057dbd7f12387d7d6c7b2adf5d54db6251
Joost De Cock <joost@joost.at>
2025-01-17 08:42:05 +0100

    wip: Work on on-demand workflows

--- .github/workflows/run-e2e-tests.sh
+++ .github/workflows/run-e2e-tests.sh
@@ -4,9 +4,12 @@ source .github/workflows/ci-comment-lib.sh
 # Exit on all errors
 set -e
 
+# When you re-run an action on GitHub, the pull request number is not available
+# So instead, we parse it out of $GITHUB_REF with this one-liner
+export PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
+
 # Create the intial comment to inform the user
-echo "folder:"
-pwd
-ls -l
+echo "GITHUB_REF: $GITHUB_REF"
+echo "PR_NUMBER: $PR_NUMBER"
 createComment
 
