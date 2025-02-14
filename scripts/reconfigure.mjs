c7844479403f916461fcf6e0e1a5b3d83d701728
levente232 <leventebajusz614@gmail.com>
2025-02-14 13:02:52 -0500

    remove token for test.

--- scripts/reconfigure.mjs
+++ scripts/reconfigure.mjs
@@ -116,7 +116,6 @@ ${(config[name][env].container?.labels || []).map((lab) => `  -l "${lab.split('`
   -e MORIO_DOCKER_LOG_DRIVER=${MORIO_DOCKER_LOG_DRIVER} \\
   -e GIT_COMMIT_SHA=${process.env['GIT_COMMIT_SHA']} \\
   -e GITHUB_PR_NUMBER=${process.env['GITHUB_PR_NUMBER']} \\
-  -e CODECOV_TOKEN=${process.env['CODECOV_TOKEN']} \\
   -e MORIO_FQDN=${process.env['MORIO_FQDN']} \\
   -e NODE_ENV=${presetGetters[env]('NODE_ENV')} \\
 ${MORIO_DOCKER_ADD_HOST ? '-e MORIO_DOCKER_ADD_HOST="' + MORIO_DOCKER_ADD_HOST + '"' : ''} \\
