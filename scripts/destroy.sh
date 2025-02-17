76c95713973831d5e9bc70dc40846b7277acaebe
levente232 <leventebajusz614@gmail.com>
2025-02-17 12:49:16 -0500

    return true in any case.

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -17,10 +17,10 @@ docker rm -fv \
   morio-web \
   morio-tap \
   morio-cache \
-  2> /dev/null
+  2> /dev/null || true
 docker network rm morionet || true
 sudo rm -rf ${MORIO_GIT_ROOT}/data/* &> /dev/null || true
 # Also remove auto-generated files
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/audit/config-template.yml &> /dev/null
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/logs/config-template.yml &> /dev/null
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/metrics/config-template.yml &> /dev/null
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/audit/config-template.yml &> /dev/null || true
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/logs/config-template.yml &> /dev/null || true
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/metrics/config-template.yml &> /dev/null || true
