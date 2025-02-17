49ba0049a536c75ac21918ab3c26a4acb4a58948
levente232 <leventebajusz614@gmail.com>
2025-02-17 13:12:13 -0500

    add debug step.

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-# Sounce config variables
+# Enable debug mode
+set -x
+
+# Source config variables
+echo "Sourcing config variables..."
 source config/cli.sh
 
+echo "Removing Docker containers..."
 docker rm -fv \
   morio-core \
   morio-api \
@@ -18,9 +23,18 @@ docker rm -fv \
   morio-tap \
   morio-cache \
   2> /dev/null || true
+
+echo "Removing Docker network..."
 docker network rm morionet || true
+
+echo "Cleaning up data folder..."
 sudo rm -rf ${MORIO_GIT_ROOT}/data/* &> /dev/null || true
+
 # Also remove auto-generated files
+echo "Removing auto-generated files..."
 rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/audit/config-template.yml &> /dev/null || true
 rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/logs/config-template.yml &> /dev/null || true
 rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/metrics/config-template.yml &> /dev/null || true
+
+# Disable debug mode
+set +x
