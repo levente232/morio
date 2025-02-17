976d89f23841500b8707440f92a0a47d892772ec
levente232 <leventebajusz614@gmail.com>
2025-02-17 15:32:47 -0500

    disable error exit on destroy.

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Enable debug mode
-set -x
+set +e
 
 # Source config variables
 echo "Sourcing config variables..."
@@ -22,19 +22,18 @@ docker rm -fv \
   morio-web \
   morio-tap \
   morio-cache \
-  2> /dev/null || true
+  2> /dev/null
 
-# echo "Removing Docker network..."
-# docker network rm morionet || true
+echo "Removing Docker network..."
+docker network rm morionet
 
-# echo "Cleaning up data folder..."
-# sudo rm -rf ${MORIO_GIT_ROOT}/data/* &> /dev/null || true
+echo "Cleaning up data folder..."
+sudo rm -rf ${MORIO_GIT_ROOT}/data/* &> /dev/null
 
 # Also remove auto-generated files
 echo "Removing auto-generated files..."
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/audit/config-template.yml &> /dev/null || true
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/logs/config-template.yml &> /dev/null || true
-rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/metrics/config-template.yml &> /dev/null || true
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/audit/config-template.yml &> /dev/null
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/logs/config-template.yml &> /dev/null
+rm -f ${MORIO_GIT_ROOT}/clients/linux/etc/morio/metrics/config-template.yml &> /dev/null
 
-# Disable debug mode
-set +x
+set -e
